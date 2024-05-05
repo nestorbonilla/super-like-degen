9632e3bbc57b23b31886ca2956b46ba461f9b51b
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-05-04 19:34:47 -0700

    tmp openrank

--- frames/api/dataapi.ts
+++ frames/api/dataapi.ts
@@ -47,13 +47,29 @@ dataApiApp.hono.get("/tax/:fid", async (c) => {
   return c.json(taxData)
 })
 
-dataApiApp.hono.get("/global-engagement/:fid", async (c) => {
-  const { fid } = c.req.param()
+dataApiApp.hono.get("/global-engagement/:fid", async (_c) => {
+  const { fid } = _c.req.param()
   const data = await getAttestations()
   const giveData = await getGiveData(fid, data)
   const receiveData = await getReceiveData(fid, data)
   const taxData = await getTaxData(fid, data)
 
   const openRankData = await getGrobalEngagementRank({ fids: [fid] })
-  return c.json(openRankData)
+
+  const [a, b, c, d, e, f, g, h, i] = [15, 0.1, 5, 1, 1, 5, 1, 1, 1]
+
+  const multipliedData = openRankData.result.map((rank) => {
+    const Adjusted_Score =
+      rank.score *
+      (1 +
+        a * (1 / (1 + Math.exp(-b * (giveData.count - c)))) +
+        d * (1 / (1 + Math.exp(-e * (receiveData.count - f)))) +
+        g * (1 / (1 + Math.exp(-h * (taxData[0].tax_amount - i)))))
+    return {
+      ...rank,
+      score: Adjusted_Score,
+    }
+  })
+
+  return _c.json(multipliedData)
 })
